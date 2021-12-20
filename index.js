246546476a928e909dbc44563ae5fba255a6d153
AnushervonKhidirov <ankhid98@gmail.com>
2021-12-20 16:24:00 +0500

    remake to OOP and added images

--- index.js
+++ index.js
@@ -1,134 +1,141 @@
-const startBtn = document.querySelector('button');
+const startGame = document.querySelector('.start_game');
+const gameTable = document.querySelector('.game-table');
 const timer = document.querySelector('.timer');
-const gameTable = document.querySelector('.game-tambel');
-let compareArr = [];
-let pairAmount = 4;
-let foundedPair = 0;
-let interval;
-let gameTimeout = 20;
+const images = ['dolphin', 'fox', 'horse', 'jaguar', 'rhinoceros']
 
-startBtn.onclick = () => {
-    clearTable();
-    createTable(pairAmount);
-}
+class Game {
+    constructor( {pairAmount, timeout} ) {
+        this.pairAmount = pairAmount;
+        this.timeout = timeout;
+        this.isFreezed = false;
+        this.foundedPair = 0;
+        this.compareArr = new Array();
+        this.interval;
+        this.images = this.shuffleArray(images);
+    }
 
-function clearTable() {
-    gameTable.innerHTML = '';
-    gameTable.classList.remove('end_game');
-    foundedPair = 0;
-    clearTimeout(timer);
-}
+    createTable() {
+        this.clearTable();
 
-function createTable(pairAmount) {
-    let suffleKeys = shuffle(createPairKey(pairAmount));
-    appendCells(pairAmount, suffleKeys);
+        let keys = this.shuffleArray(this.createPairKey())
+        this.gameTimer();
 
-    timer.innerHTML = gameTimeout;
-
-    interval = setInterval(() => {
-        gameTimeout--;
-
-        if (gameTimeout < 10) gameTimeout = '0' + gameTimeout.toString();
-        timer.innerHTML = gameTimeout;
-
-        if (gameTimeout == 0) {
-            endGame(false);
-            clearInterval(interval);
+        for (let i = 0; i < this.pairAmount * 2; i++) {
+            gameTable.appendChild(this.createCells(i, keys));
         }
-    }, 1000);
-}
+    }
 
-function appendCells(pairAmount, keys) {
-    for (let i = 0; i < pairAmount * 2; i++) {
+    createCells(index, keys) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.dataset.key = keys[i];
-        cell.id = i;
-        cell.onclick = (e) => addToCompare(e);
-        gameTable.appendChild(cell);
+        cell.dataset.key = keys[index];
+        cell.style.backgroundImage = `url(images/${images[keys[index]]}.png)`
+        cell.id = index;
+        cell.onclick = (e) => this.addToCompare(e);
+        return cell;
+    }
+
+    createPairKey() {
+        let pairArray = [];
+
+        for (let i = 0; i < this.pairAmount; i++) {
+            pairArray.push(i);
+            pairArray.push(i);
+        }
+
+        return pairArray;
+    }
+
+    shuffleArray(array) {
+        let currentIndex = array.length,
+            randomIndex;
+
+        while (currentIndex != 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
+    addToCompare(e) {
+        if (this.isFreezed) return;
+
+        let cell = e.target;
+        let cellAttr = {
+            id: cell.id,
+            key: cell.dataset.key,
+            elem: cell
+        }
+    
+        cell.classList.add('show');
+        this.compareArr.push(cellAttr);
+
+        if (this.compareArr.length == 2) {
+            this.isFreezed = true;
+            this.compareCells();
+        };
+    }
+
+    compareCells() {
+        let cell_1 = this.compareArr[0];
+        let cell_2 = this.compareArr[1];
+
+        if (cell_1.id !== cell_2.id && cell_1.key === cell_2.key) {
+            this.foundedPair++
+            if (this.foundedPair == this.pairAmount) this.endGame(true);
+            setTimeout(() => this.successfulComp(), 500);
+        } else if (cell_1.id === cell_2.id) {
+            this.clearCompArr();
+        } else {
+            setTimeout(() => this.clearCompArr(), 500);
+        }
+    }
+
+    successfulComp() {
+        this.compareArr[0].elem.classList.add('fonded');
+        this.compareArr[1].elem.classList.add('fonded');
+        setTimeout(() => this.clearCompArr(), 200);
+    }
+
+    clearCompArr() {
+        this.isFreezed = false;
+        this.compareArr[0].elem.classList.remove('show');
+        this.compareArr[1].elem.classList.remove('show');
+        this.compareArr = [];
+    }
+
+    gameTimer() {
+        timer.innerHTML = this.timeout;
+        let time = this.timeout;
+
+        this.interval = setInterval(() => {
+            time--;
+
+            if (time < 10) time = `0${time}`;
+
+            timer.innerHTML = time;
+            if (time == 0) this.endGame();
+        }, 1000);
+    }
+
+    endGame(isWin) {
+        isWin ? alert('you win') : alert('you lose');
+        gameTable.classList.add('end_game');
+        this.foundedPair = 0;
+        clearInterval(this.interval);
+    }
+
+    clearTable() {
+        gameTable.innerHTML = '';
+        gameTable.classList.remove('end_game');
+        this.foundedPair = 0;
+        this.timeout = this.timeout
+        clearInterval(this.interval);
     }
 }
 
-function createPairKey(pairAmount) {
-    let pairArray = [];
 
-    for (let i = 0; i < pairAmount; i++) {
-        pairArray.push(i);
-        pairArray.push(i);
-    }
-
-    return pairArray;
-}
-
-function shuffle(array) {
-    let currentIndex = array.length,
-        randomIndex;
-
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
-}
-
-function addToCompare(e) {
-    let cell = e.target;
-    let cellAttr = {
-        id: cell.id,
-        key: cell.dataset.key,
-        elem: cell
-    }
-
-    cell.classList.add('show');
-    compareArr.push(cellAttr);
-    if (compareArr.length == 2) {
-        gameTable.classList.add('disabled');
-        copmairCells()
-    };
-}
-
-function copmairCells() {
-    let cellId1 = compareArr[0].id;
-    let cellId2 = compareArr[1].id;
-    let cellKey1 = compareArr[0].key;
-    let cellKey2 = compareArr[1].key;
-
-    if (cellId1 !== cellId2 && cellKey1 === cellKey2) {
-        foundedPair++
-
-        if (foundedPair == pairAmount) endGame(true);
-
-        setTimeout(() => {
-            compareArr[0].elem.classList.add('fonded');
-            compareArr[1].elem.classList.add('fonded');
-            gameTable.classList.remove('disabled');
-            setTimeout(() => cleareCompArr(), 200);
-        }, 500);
-
-    } else if (cellId1 === cellId2) {
-        cleareCompArr();
-    } else {
-        setTimeout(() => cleareCompArr(), 500);
-    }
-
-    function cleareCompArr() {
-        gameTable.classList.remove('disabled');
-        compareArr[0].elem.classList.remove('show');
-        compareArr[1].elem.classList.remove('show');
-        compareArr = [];
-    }
-}
-
-function endGame(win) {
-    if (win) {
-        console.log('you win');
-    } else {
-        console.log('you lose');
-    }
-
-    gameTable.classList.add('end_game');
-    foundedPair = 0;
-}
+let game = new Game( {pairAmount: 4, timeout: 20} );
+startGame.onclick = () => game.createTable();
